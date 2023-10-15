d0ad2951923c8dd5259049b0138979aa406d3295
Codechickdev <codechickdev12@gmail.com>
2023-10-15 20:29:56 +0530

    Added Full Functionality

--- src/hashmap.py
+++ src/hashmap.py
@@ -12,32 +12,33 @@ except ModuleNotFoundError or ImportError as e:
 
 
 class Hashmap:
-    def __init__(self, path: Path) -> None:
-        self.path = path
+    def __init__(self, path: Path, search_path: Path) -> None:
+        self.data_file_path = path
+        self.search_path = search_path
         return None
 
-    def find_json_file(self, data_file_path) -> bool:
-        return os.path.exists(data_file_path)
+    def find_json_file(self) -> bool:
+        return os.path.exists(self.data_file_path)
 
-    def load_json_file(self, data_file_path) -> dict:
+    def load_json_file(self) -> dict:
         try:
-            with open(data_file_path, "r") as json_file:
+            with open(self.data_file_path, "r") as json_file:
                 file_list = json.load(json_file)
             return file_list
         except FileNotFoundError as e:
             return f"Error in Loading Json File: File Not Found"
 
-    def create_json_file(self, search_path, data_file_path) -> dict:
-        if not self.find_json_file(data_file_path):
+    def create_json_file(self) -> dict:
+        if not self.find_json_file():
             file_list = {}
 
-            for root, _, files in os.walk(search_path):
+            for root, _, files in os.walk(self.search_path):
                 for file in files:
                     file_list[file] = os.path.join(root, file)
 
-            with open(data_file_path, "w") as json_file:
+            with open(self.data_file_path, "w") as json_file:
                 json.dump(file_list, json_file)
 
             return file_list
         else:
-            return self.load_json_file(data_file_path)
+            return self.load_json_file()
--- src/tk_window.py
+++ src/tk_window.py
@@ -1,32 +1,31 @@
-import os
 import tkinter as tk
 from tkinter import ttk
+from pathlib import Path
+
+from optimization import search_file
+
+
+def clear_inputs():
+    search_entry.delete(0, tk.END)
+    # show_files_var.set(False)
+    # show_folders_var.set(False)
+    extension_entry.delete(0, tk.END)
+    # is_folder.set(False)
+    result_listbox.delete(0)
 
 
 def search_files_folders():
     search_term = search_entry.get()
-    show_files = show_files_var.get()
-    show_folders = show_folders_var.get()
+    # show_files = show_files_var.get()
+    # show_folders = show_folders_var.get()
     extension_filter = extension_entry.get()
 
     result_listbox.delete(0, tk.END)
+    file_to_search = search_term + extension_filter
 
-    for root, dirs, files in os.walk(os.getcwd()):
-        if show_folders:
-            for dir_name in dirs:
-                if search_term.lower() in dir_name.lower():
-                    result_listbox.insert(
-                        tk.END, f"Folder: {os.path.join(root, dir_name)}"
-                    )
+    result = Path(search_file(file_to_search))
 
-        if show_files:
-            for file_name in files:
-                if search_term.lower() in file_name.lower() and (
-                    not extension_filter or file_name.endswith(extension_filter)
-                ):
-                    result_listbox.insert(
-                        tk.END, f"File: {os.path.join(root, file_name)}"
-                    )
+    result_listbox.insert(tk.END, f"File: {result}")
 
 
 root = tk.Tk()
@@ -42,16 +41,20 @@ search_label.grid(row=0, column=0, sticky="w")
 search_entry = ttk.Entry(search_frame, width=30)
 search_entry.grid(row=0, column=1, padx=(10, 10))
 
-show_files_var = tk.BooleanVar()
-show_files_checkbox = ttk.Checkbutton(search_frame, text="File", variable=show_files_var)
-show_files_checkbox.grid(row=0, column=2)
+# show_files_var = tk.BooleanVar()
+# show_files_checkbox = ttk.Checkbutton(
+#     search_frame, text="File", variable=show_files_var
+# )
+# show_files_checkbox.grid(row=0, column=2)
 
-show_folders_var = tk.BooleanVar()
-show_folders_checkbox = ttk.Checkbutton(search_frame, text="Folder", variable=show_folders_var)
-show_folders_checkbox.grid(row=0, column=3)
-is_folder = tk.BooleanVar()
-is_folder_checkbox = ttk.Checkbutton(search_frame, text="Is Folder", variable=is_folder)
-is_folder_checkbox.grid(row=0, column=3, padx=(10, 10))
+# show_folders_var = tk.BooleanVar()
+# show_folders_checkbox = ttk.Checkbutton(
+#     search_frame, text="Folder", variable=show_folders_var
+# )
+# show_folders_checkbox.grid(row=0, column=3)
+# is_folder = tk.BooleanVar()
+# is_folder_checkbox = ttk.Checkbutton(search_frame, text="Is Folder", variable=is_folder)
+# is_folder_checkbox.grid(row=0, column=3, padx=(10, 10))
 
 extension_label = ttk.Label(search_frame, text="Extension:")
 extension_label.grid(row=0, column=4, padx=(10, 0))
@@ -64,5 +67,8 @@ search_button.grid(row=0, column=6, padx=(10, 10))
 
 result_listbox = tk.Listbox(root, width=80, height=20)
 result_listbox.grid(row=1, column=0, padx=10, pady=10)
+
+clear_button = ttk.Button(search_frame, text="Clear", command=clear_inputs)
+clear_button.grid(row=0, column=7, padx=(10, 10))
 
 root.mainloop()
